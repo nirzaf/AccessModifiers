ef663f63c24f95afb6251bb141f4d7fadf754be0
nirzaf <mfmfazrin1986@gmail.com>
2022-02-15 10:12:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿global using static System.Console;
+
+
+Water water = new();
+
+water.Drink("fazrin");
+WriteLine(water.DrinkInternal());
+
+Juice juice = new();
+
+juice.Drink("Drinking Juice");
+WriteLine(juice.DrinkInternal());
+
+public class Water
+{
+    public void Drink(string name)
+    {
+        Drink();
+        WriteLine(name + "is drinking water!");
+    }
+
+    private void Drink()
+    {
+        WriteLine("Drinking water!");
+    }
+
+
+    protected string DrickProtected()
+    {
+        return "Protected Drinking water!";
+    }
+
+    internal string DrinkInternal()
+    {
+        return "Internal Drinking water!";
+    }
+}
+
+public class Juice : Water
+{
+    public void Make()
+    {
+        WriteLine("Add juice powder");
+    }
+}
